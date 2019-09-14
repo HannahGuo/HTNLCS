8f9b7a6d3874815233b95bca2a265374e7b6e042
WillQi <williamqipizza@gmail.com>
2019-09-14 16:14:21 -0600

    kill me now

--- client/src/components/Body/BodyBot.js
+++ client/src/components/Body/BodyBot.js
@@ -7,25 +7,11 @@ export default class BodyBot extends Component {
     return (
       <div className="all">
         <div className={style.hello}>
-          <h1>Welcome to LCS...</h1>
-          <span>
-            A conversational tool that allows you to practice speaking different
-            languages, while also tracking statistics of your conversation in order to help you
-            improve.
-            <br />
-            <br />
-            Simply access the Linguisbit tab in the menu above to begin chatting with the bot.
-            <br />
-            <br />
-            The My Profile Section in the menu above will allow you view all of your past
-            conversations.
-          </span>
-          <br />
-          <br />
-          <h2>Happy Langauge Practicing!</h2>
-          <br />
-          <br />
-          Made with ❤️ using ReactJS at Hack the North 2019 by Hannah Guo and William Qi.
+          <div className={style.messageBox}></div>
+          <div className={style.chatBox}>
+            <input className={style.chatBoxText} id="chatInput" />
+            <button className={style.chatBoxButton} id="sendChatButton">Send</button>
+          </div>
         </div>
       </div>
     );
