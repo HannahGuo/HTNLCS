c43c0a118126495581502089152382ff4a6fd456
WillQi <williamqipizza@gmail.com>
2019-09-15 00:28:18 -0600

    IT IS DONE

--- client/public/bot.js
+++ client/public/bot.js
@@ -3,7 +3,7 @@ const database = firebase.database();
 const chatTable = database.ref("chat");
 
 const username = localStorage.getItem("username");
-const channel = "english";
+let channel = "";
 
 
 const styles = {
@@ -16,83 +16,102 @@ let conversation = "";
 
 window.addEventListener("load", () => {
 
-    chatTable.push({
-        message: "",
-        author: username,
-        type: "join",
-        channel, //TODO
-    });
 
-    const sendChatMessage = async () => {
-        const value = document.querySelector("#chatInput").value;
-        document.querySelector("#chatInput").value = "";
-        if (value.length === 0) return;
-        if (value.toLocaleLowerCase() === "/done") { // We're done. Let's analyze our data.
+    Array.from(document.querySelectorAll("div[data-language]")).map(el => el.addEventListener("click", e => {
+        const target = e.target.tagName === "DIV" ? e.target : e.target.parentElement;
+        channel = target.getAttribute("data-language");
 
-            chatTable.push({
-                message: "",
-                author: username,
-                type: "leave",
-                channel, //TODO
-            });
-            document.querySelector("#chatInput").disabled = true;
 
-            const data = await LanguageAPI.grade({
-                channel,
-                username,
-                text: conversation,
-                language: channel
-            });
-            
-            // TODO: DO SOMETHING WITH THAT DATA.
-            console.log(data);
-            return;
-        }
-        conversation += value + " ";
+        document.querySelector("div[data-tab='chat']").removeAttribute("data-hidden");
+        document.querySelector("div[data-tab='language']").setAttribute("data-hidden", "");
+        document.querySelector("h2[data-key='language']").textContent = channel.slice(0, 1).toLocaleUpperCase() + channel.slice(1);
+        document.querySelector("input[data-key='input']").placeholder = `What do you want to say in ${channel.slice(0, 1).toLocaleUpperCase() + channel.slice(1)}?`;
+
+        window.scrollTo(0, document.body.scrollHeight);
+
+
+
         chatTable.push({
-            message: value,
+            message: "",
             author: username,
-            type: "message",
+            type: "join",
             channel, //TODO
         });
-    };
-
-    chatTable.on("value", snapshot => {
-
-        
-
-        const messages = snapshot.val();
-        for (const id in messages) {
-            if (!document.querySelector(`div[message-id="${id}"]`) && messages[id].channel === channel) {
-                if (messages[id].type === "message") {
-                    const messageEl = document.createElement("div");
-                    messageEl.setAttribute("message-id", id);
-                    messageEl.setAttribute("style", messages[id].author === username ? styles.ours : styles.theirs);
-                    messageEl.textContent = messages[id].message;
-                    document.querySelector("#chatBox").appendChild(messageEl);
-                } else if (messages[id].type === "leave") {
-                    const messageEl = document.createElement("div");
-                    messageEl.setAttribute("message-id", id);
-                    messageEl.setAttribute("style", styles.line);
-                    messageEl.textContent = `${messages[id].author} has ended their conversation.`;
-                    document.querySelector("#chatBox").appendChild(messageEl);
-                } else {
-                    const messageEl = document.createElement("div");
-                    messageEl.setAttribute("message-id", id);
-                    messageEl.setAttribute("style", styles.line);
-                    messageEl.textContent = `${messages[id].author} has joined the conversation.`;
-                    document.querySelector("#chatBox").appendChild(messageEl);
-                }
-                const breakEl = document.createElement("div");
-                    breakEl.setAttribute("style", styles.line);
-                    document.querySelector("#chatBox").appendChild(breakEl);
-                    document.querySelector("#chatBox").scrollTop = document.querySelector("#chatBox").scrollHeight;
+    
+        const sendChatMessage = async () => {
+            const value = document.querySelector("#chatInput").value;
+            document.querySelector("#chatInput").value = "";
+            if (value.length === 0) return;
+            if (value.toLocaleLowerCase() === "/done") { // We're done. Let's analyze our data.
+    
+                chatTable.push({
+                    message: "",
+                    author: username,
+                    type: "leave",
+                    channel, //TODO
+                });
+                document.querySelector("#chatInput").disabled = true;
+    
+                const data = await LanguageAPI.grade({
+                    channel,
+                    username,
+                    text: conversation,
+                    language: channel
+                });
+                
+                // TODO: DO SOMETHING WITH THAT DATA.
+                console.log(data);
+                return;
             }
-        }
-    });
+            conversation += value + " ";
+            chatTable.push({
+                message: value,
+                author: username,
+                type: "message",
+                channel, //TODO
+            });
+        };
+    
+        chatTable.on("value", snapshot => {
+    
+            
+    
+            const messages = snapshot.val();
+            for (const id in messages) {
+                if (!document.querySelector(`div[message-id="${id}"]`) && messages[id].channel === channel) {
+                    if (messages[id].type === "message") {
+                        const messageEl = document.createElement("div");
+                        messageEl.setAttribute("message-id", id);
+                        messageEl.setAttribute("style", messages[id].author === username ? styles.ours : styles.theirs);
+                        messageEl.textContent = messages[id].message;
+                        document.querySelector("#chatBox").appendChild(messageEl);
+                    } else if (messages[id].type === "leave") {
+                        const messageEl = document.createElement("div");
+                        messageEl.setAttribute("message-id", id);
+                        messageEl.setAttribute("style", styles.line);
+                        messageEl.textContent = `${messages[id].author} has ended their conversation.`;
+                        document.querySelector("#chatBox").appendChild(messageEl);
+                    } else {
+                        const messageEl = document.createElement("div");
+                        messageEl.setAttribute("message-id", id);
+                        messageEl.setAttribute("style", styles.line);
+                        messageEl.textContent = `${messages[id].author} has joined the conversation.`;
+                        document.querySelector("#chatBox").appendChild(messageEl);
+                    }
+                    const breakEl = document.createElement("div");
+                        breakEl.setAttribute("style", styles.line);
+                        document.querySelector("#chatBox").appendChild(breakEl);
+                        document.querySelector("#chatBox").scrollTop = document.querySelector("#chatBox").scrollHeight;
+                }
+            }
+        });
+    
+        document.querySelector("#sendChatButton").addEventListener("click", sendChatMessage);
+        document.querySelector("#chatInput").addEventListener("keydown", e => {
+            if (e.keyCode === 13) sendChatMessage();
+        });
 
-    document.querySelector("#sendChatButton").addEventListener("click", sendChatMessage);
-    document.querySelector("#chatInput").addEventListener("keydown", e => {
-        if (e.keyCode === 13) sendChatMessage();
-    });
+    }));
+
+    
 });
--- client/src/components/Body/BodyBot.js
+++ client/src/components/Body/BodyBot.js
@@ -7,11 +7,28 @@ export default class BodyBot extends Component {
     return (
       <div className="all">
         <div className={style.hello}>
-          <h2>English</h2> <span>Want to be evalulated on your linguistic skills? Type <i>/done</i></span>
-          <div className={style.messageBox} id="chatBox"></div>
-          <div className={style.chatBox}>
-            <input className={style.chatBoxText} id="chatInput" placeholder="What do you want to say in English?" />
-            <button className={style.chatBoxButton} id="sendChatButton">Send</button>
+          <div data-tab="language" >
+            <h2 style={{"textAlign": "center"}}>Choose a Language</h2>
+            <div className={style.cards}>
+              <div className={style.card} data-language="english">
+                <h3>English</h3>
+              </div>
+              <div className={style.card} data-language="french">
+                <h3>French</h3>
+              </div>
+              <div className={style.card} data-language="spanish">
+                <h3>Spanish</h3>
+              </div>
+            </div>
+          </div>
+
+          <div data-tab="chat" data-hidden>
+            <h2 data-key="language">English</h2> <span>Want to be evalulated on your linguistic skills? Type <i>/done</i></span>
+            <div className={style.messageBox} id="chatBox"></div>
+            <div className={style.chatBox}>
+              <input className={style.chatBoxText} id="chatInput" data-key="input" placeholder="What do you want to say in English?" />
+              <button className={style.chatBoxButton} id="sendChatButton">Send</button>
+            </div>
           </div>
         </div>
       </div>
